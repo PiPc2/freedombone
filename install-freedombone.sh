6d80ad4edf0efe6a617345b088a8d171567abd64
Bob Mottram <bob@robotics.uk.to>
2014-09-21 21:31:22 +0100

    All ephemeral ports

--- install-freedombone.sh
+++ install-freedombone.sh
@@ -442,13 +442,13 @@ function save_firewall_settings {
   chmod +x /etc/network/if-up.d/iptables
 }
 
-function configure_firewall_for_ftp {
-  if grep -Fxq "configure_firewall_for_ftp" $COMPLETION_FILE; then
+function configure_firewall_ephemeral_ports {
+  if grep -Fxq "configure_firewall_ephemeral_ports" $COMPLETION_FILE; then
 	  return
   fi
-  iptables -I INPUT -i eth0 -p tcp --dport 49152:65534 -j ACCEPT
+  iptables -I INPUT -i eth0 -p tcp --dport 32768:61000 -j ACCEPT
   save_firewall_settings
-  echo 'configure_firewall_for_ftp' >> $COMPLETION_FILE
+  echo 'configure_firewall_ephemeral_ports' >> $COMPLETION_FILE
 }
 
 function configure_firewall_for_web {
@@ -953,7 +953,7 @@ enable_zram
 random_number_generator
 configure_firewall
 configure_firewall_for_web
-configure_firewall_for_ftp
+configure_firewall_ephemeral_ports
 configure_firewall_for_git
 configure_firewall_for_ssh
 configure_firewall_for_email
