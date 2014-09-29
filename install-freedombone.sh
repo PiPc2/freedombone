33fb8464ec9cbd8617cad180776773a60acd6859
Bob Mottram <bob@robotics.uk.to>
2014-09-29 09:40:26 +0100

    Add a license to the script

--- install-freedombone.sh
+++ install-freedombone.sh
@@ -13,8 +13,27 @@
 # Please note that the various hashes and download archives
 # for systems such as Owncloud and Dokuwiki may need to be updated
 #
+# License
+# =======
+#
+# Copyright (C) 2014 Bob Mottram <bob@robotics.uk.to>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 # Summary
 # =======
+#
 # To be able to run this script you need to get to a condition
 # where you have Debian Jessie installed, with at least one
 # unprivileged user account and at least one subdomain created on
