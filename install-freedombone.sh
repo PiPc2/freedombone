7eb0f1d6272b4e09ec8a0a4be231c8d1dd9e0f45
Bob Mottram <bob@robotics.uk.to>
2014-09-30 19:05:24 +0100

    tidying

--- install-freedombone.sh
+++ install-freedombone.sh
@@ -267,14 +267,15 @@ function argument_checks {
 
 function check_hwrng {
     # If hardware random number generation was enabled then make sure that the device exists.
-    # if /dev/hwrng is not found than any subsequent cryptographic key generation would
+    # if /dev/hwrng is not found then any subsequent cryptographic key generation would
     # suffer from low entropy and might be insecure
     if [ ! -f /etc/default/rng-tools ]; then
         return
     fi
     if [ ! -b /dev/hwrng ]; then
         ls /dev/hw*
-        echo 'The hardware random number generator is anabled but could not be detected on /dev/hwrng.  There may be a problem with the installation or the Beaglebone hardware.'
+        echo 'The hardware random number generator is anabled but could not be detected on'
+		echo '/dev/hwrng.  There may be a problem with the installation or the Beaglebone hardware.'
         exit 75
     fi
 }
