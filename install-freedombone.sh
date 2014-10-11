f1a691b759c9ebece25630a5eb5a7e0624be9ab7
Bob Mottram <bob@robotics.uk.to>
2014-10-11 19:31:31 +0100

    Quotes

--- install-freedombone.sh
+++ install-freedombone.sh
@@ -3862,8 +3862,8 @@ function install_blog {
 
   # create a user
   cp /var/www/$FULLBLOG_DOMAIN_NAME/htdocs/config/users/username.ini.example /var/www/$FULLBLOG_DOMAIN_NAME/htdocs/config/users/$MY_USERNAME.ini
-  HASHED_BLOG_PASSWORD=$(sha256sum $FULLBLOG_ADMIN_PASSWORD)
-  sed -i "s/yourpassword/$HASHED_BLOG_PASSWORD/g" /var/www/$FULLBLOG_DOMAIN_NAME/htdocs/config/users/$MY_USERNAME.ini
+  HASHED_BLOG_PASSWORD="$(sha256sum $FULLBLOG_ADMIN_PASSWORD)"
+  sed -i "s|yourpassword|'$HASHED_BLOG_PASSWORD'|g" /var/www/$FULLBLOG_DOMAIN_NAME/htdocs/config/users/$MY_USERNAME.ini
   sed -i 's/encryption = clear/encryption = sha256/g' /var/www/$FULLBLOG_DOMAIN_NAME/htdocs/config/users/$MY_USERNAME.ini
 
   nginx_ensite $FULLBLOG_DOMAIN_NAME
