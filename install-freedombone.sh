89667379a5a39e0eebdb7e3530f6465b97bff9c5
Bob Mottram <bob@robotics.uk.to>
2015-01-04 11:40:46 +0000

    Dates changed

--- install-freedombone.sh
+++ install-freedombone.sh
@@ -16,7 +16,7 @@
 # License
 # =======
 #
-# Copyright (C) 2014 Bob Mottram <bob@robotics.uk.to>
+# Copyright (C) 2014-2015 Bob Mottram <bob@robotics.uk.to>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
