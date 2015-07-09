d4fcec5b9fa5f712c4dccaf025769a3af36f70ae
Bob Mottram <bob@robotics.uk.to>
2015-07-09 18:55:52 +0100

    debian build script fails more gracefully

--- debian.sh
+++ debian.sh
@@ -2,7 +2,7 @@
 
 APP=freedombone
 PREV_VERSION=1.00
-VERSION=1.00
+VERSION=1.01
 ARCH_TYPE="all"
 DIR=${APP}-${VERSION}
 
@@ -13,7 +13,10 @@ sed -i 's/VERSION="'${PREV_VERSION}'"/VERSION="'${VERSION}'"/g' src/freedombone-
 sed -i 's/VERSION="'${PREV_VERSION}'"/VERSION="'${VERSION}'"/g' src/freedombone-client
 
 # change the parent directory name to debian format
+cp releases/* ..
+mv releases /tmp/freedombone
 mv ../${APP} ../${DIR}
+mkdir /tmp/freedombone
 
 # Create a source archive
 make clean
@@ -21,10 +24,29 @@ make source
 
 # Build the package
 dpkg-buildpackage -F
+if [ ! "$?" = "0" ]; then
+    mv ../${DIR} ../${APP}
+    mv /tmp/freedombone/releases .
+    exit 478
+fi
 
 # sign files
 gpg -ba ../${APP}_${VERSION}-1_${ARCH_TYPE}.deb
+if [ ! "$?" = "0" ]; then
+    mv ../${DIR} ../${APP}
+    mv /tmp/freedombone/releases .
+    exit 639
+fi
+
 gpg -ba ../${APP}_${VERSION}.orig.tar.gz
+if [ ! "$?" = "0" ]; then
+    mv ../${DIR} ../${APP}
+    mv /tmp/freedombone/releases .
+    exit 592
+fi
 
 # restore the parent directory name
 mv ../${DIR} ../${APP}
+mv /tmp/freedombone/releases .
+
+exit 0
