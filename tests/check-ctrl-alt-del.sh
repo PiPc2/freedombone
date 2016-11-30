d4c07b2cc8e5a2df28c70f1b3b870021d78faa54
Bob Mottram <bob@robotics.uk.to>
2016-11-30 16:04:05 +0000

    Improve check for ctrl-alt-del

--- tests/check-ctrl-alt-del.sh
+++ tests/check-ctrl-alt-del.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ -f /etc/systemd/system/ctrl-alt-del.target ];then
+if [ -L /etc/systemd/system/ctrl-alt-del.target ];then
     ctrl_alt_del=$(ls -l /etc/systemd/system/ctrl-alt-del.target)
     if [[ "$ctrl_alt_del" !=  *'/dev/null' ]]; then
         exit 1
     fi
 else
-    exit 1
+    if [ -f /etc/systemd/system/ctrl-alt-del.target ];then
+        exit 1
+    fi
 fi
