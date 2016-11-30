b106d148905d9b9d88886bde837f9d8a09b5e847
Bob Mottram <bob@robotics.uk.to>
2016-11-30 15:46:04 +0000

    Single quotes

--- tests/check-ctrl-alt-del.sh
+++ tests/check-ctrl-alt-del.sh
@@ -2,7 +2,7 @@
 
 if [ -f /etc/systemd/system/ctrl-alt-del.target ];then
     ctrl_alt_del=$(ls -l /etc/systemd/system/ctrl-alt-del.target)
-    if [[ "$ctrl_alt_del" !=  *"/dev/null" ]]; then
+    if [[ "$ctrl_alt_del" !=  *'/dev/null' ]]; then
         exit 1
     fi
 else
