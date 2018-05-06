719b8131576d21f273fb4120d4003a0bf0e8c800
Bob Mottram <bob@freedombone.net>
2018-05-06 10:33:53 +0100

    Two separate conditions

--- tests/check-ssh.sh
+++ tests/check-ssh.sh
@@ -47,8 +47,12 @@ case $1 in
     installed)
 	OPENSSH_SERVER=$(dpkg -s openssh-server | grep -i "Status:.*install.*ok.*installed" | wc -l)
 	OPENSSH_CLIENT=$(dpkg -s openssh-client | grep -i "Status:.*install.*ok.*installed" | wc -l)
-	if [ ${OPENSSH_SERVER} -eq 1 -a ${OPENSSH_CLIENT} -eq 1 ]; then
-	    :
+	if [ ${OPENSSH_SERVER} -eq 1 ]; then
+	    if [ ${OPENSSH_CLIENT} -eq 1 ]; then
+		:
+	    else
+		exit 1
+	    fi
 	else
 	    exit 1
 	fi	
