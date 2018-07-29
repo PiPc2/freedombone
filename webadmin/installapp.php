53ffb5816257376645a33919f9df36fe48c9bb22
Bob Mottram <bob@freedombone.net>
2018-07-29 14:59:06 +0100

    Adding output from app install button

--- webadmin/installapp.php
+++ webadmin/installapp.php
@@ -1,9 +1,10 @@
 <?php
 
+$output_filename = "apps_add.html";
+
 if (isset($_POST['install'])) {
     $app_name = htmlspecialchars($_POST['app_name']);
     $install_domain = $_POST['install_domain'];
-    echo "domain: ".$install_domain;
 
     if(! file_exists("pending_installs.txt")) {
         $pending_installs = fopen("pending_installs.txt", "w") or die("Unable to create installs file");
@@ -14,7 +15,16 @@ if (isset($_POST['install'])) {
         $pending_installs = fopen("pending_installs.txt", "a") or die("Unable to append to installs file");
         fwrite($pending_installs, "install_".$app_name."\n");
         fclose($pending_installs);
+        $output_filename = "app_installing.html";
+    }
+    else {
+        // The app is already scheduled for installation
+        $output_filename = "app_scheduled.html";
     }
 }
+
+$htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
+echo fread($htmlfile,filesize("$output_filename"));
+fclose($htmlfile);
 
 ?>
