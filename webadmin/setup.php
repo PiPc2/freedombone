94448561b06af8d5816d0f304cf17699fba3252b
Bob Mottram <bob@freedombone.net>
2018-07-30 23:35:01 +0100

    Initial setup configuration for web admin

---
+++ webadmin/setup.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['setup'])) {
+    $my_username = htmlspecialchars($_POST['my_username']);
+    $default_domain_name = htmlspecialchars($_POST['default_domain_name']);
+
+    if(!file_exists("setup.txt")) {
+        $setup_file = fopen("setup.txt", "w") or die("Unable to create setup file");
+        fwrite($setup_file, $my_username.",".$default_domain_name."\n");
+        fclose($);
+    }
+
+    $output_filename = "setup_installing.html";
+    $htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
+    echo fread($htmlfile,filesize("$output_filename"));
+    fclose($htmlfile);
+}
+
+?>
