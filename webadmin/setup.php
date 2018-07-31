169167dcfcf315414de8ea92d417184988ef8e0c
Bob Mottram <bob@freedombone.net>
2018-07-31 19:44:17 +0100

    Move to confirm screen

--- webadmin/setup.php
+++ webadmin/setup.php
@@ -1,19 +1,24 @@
 <?php
 
+$output_filename = "index.html";
+
 if (isset($_POST['setup'])) {
-    $my_username = htmlspecialchars($_POST['my_username']);
-    $default_domain_name = htmlspecialchars($_POST['default_domain_name']);
+    if(file_exists("setup_confirm.html")) {
+        $my_username = htmlspecialchars($_POST['my_username']);
+        $default_domain_name = htmlspecialchars($_POST['default_domain_name']);
 
-    if(!file_exists(".temp_setup.txt")) {
-        $setup_file = fopen(".temp_setup.txt", "w") or die("Unable to create setup file");
-        fwrite($setup_file, $my_username.",".$default_domain_name."\n");
-        fclose($);
+        if(!file_exists(".temp_setup.txt")) {
+            $setup_file = fopen(".temp_setup.txt", "w") or die("Unable to create setup file");
+            fwrite($setup_file, $my_username.",".$default_domain_name."\n");
+            fclose($);
+        }
+
+        $output_filename = "setup_confirm.html";
     }
-
-    $output_filename = "setup_installing.html";
-    $htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
-    echo fread($htmlfile,filesize("$output_filename"));
-    fclose($htmlfile);
 }
+
+$htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
+echo fread($htmlfile,filesize("$output_filename"));
+fclose($htmlfile);
 
 ?>
