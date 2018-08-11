26fab1a4613a9a707548306ad01b6357ff615b5e
Bob Mottram <bob@freedombone.net>
2018-08-11 10:49:34 +0100

    Have a sensible maximum username length

--- webadmin/newuser.php
+++ webadmin/newuser.php
@@ -7,7 +7,7 @@ $output_filename = "users.html";
 if (isset($_POST['submitnewuser'])) {
     $username = htmlspecialchars($_POST['username']);
 
-    if ((!preg_match('/[^a-z0-9]/', $username)) || (strlen($username)<4)) {
+    if ((!preg_match('/[^a-z0-9]/', $username)) || (strlen($username)<4) || (strlen($username)>32)) {
         $output_filename = "new_user_invalid.html";
     }
     else {
