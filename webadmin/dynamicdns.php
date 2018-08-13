8f298c8869c76521f8ef6fa59203bba2c10f5f15
Bob Mottram <bob@freedombone.net>
2018-08-13 20:24:57 +0100

    Link dynamic dns to the webadmin daemon

---
+++ webadmin/dynamicdns.php
@@ -0,0 +1,21 @@
+<?php
+
+// Change dynamic DNS settings
+
+$output_filename = "settings.html";
+
+if (isset($_POST['submitddns'])) {
+    $ddns = htmlspecialchars($_POST['dynamicdns']);
+    $ddns_username = htmlspecialchars($_POST['ddns_username']);
+    $ddns_password = htmlspecialchars($_POST['ddns_password']);
+
+    $ddns_file = fopen(".dynamicdns.txt", "w") or die("Unable to create setup file");
+    fwrite($ddns_file, $ddns.','.$ddns_username.','.$ddns_password);
+    fclose($ddns_file);
+}
+
+$htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
+echo fread($htmlfile,filesize("$output_filename"));
+fclose($htmlfile);
+
+?>
