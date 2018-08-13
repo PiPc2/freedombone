35f9a929f505ced2b0406d86494d94873884a5f8
Bob Mottram <bob@freedombone.net>
2018-08-13 22:35:43 +0100

    Remain on screen when update button is pressed

--- webadmin/systemmonitor.php
+++ webadmin/systemmonitor.php
@@ -8,6 +8,7 @@ if (isset($_POST['submitsystemmonitor'])) {
     $system_monitor_file = fopen(".system_monitor.txt", "w") or die("Unable to create setup file");
     fwrite($system_monitor_file, " ");
     fclose($system_monitor_file);
+    $output_filename = "system_monitor.html";
 }
 
 $htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
