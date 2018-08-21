38284dedc78f80b0ebf90bb00f1b4d3c1831a681
Bob Mottram <bob@freedombone.net>
2018-08-21 13:06:17 +0100

    pleroma settings in web ui

---
+++ webadmin/settings_pleroma.php
@@ -0,0 +1,30 @@
+<?php
+
+// Pleroma settings menu
+
+$output_filename = "settings_pleroma.html";
+
+if (isset($_POST['submitallowregistrations'])) {
+    $confirm = htmlspecialchars($_POST['allowregistrations']);
+    $settings_file = fopen(".appsettings.txt", "w") or die("Unable to write to appsettings file");
+    fwrite($setting_file, "pleroma,registration,".$confirm);
+    fclose($setting_file);
+}
+
+if (isset($_POST['submitemoji'])) {
+    $shortcode = htmlspecialchars($_POST['emoji_shortcode']);
+    if (strpos($shortcode, ' ') === false) {
+        $url = htmlspecialchars($_POST['emoji_url']);
+        if (strpos($url, ' ') === false) {
+            $settings_file = fopen(".appsettings.txt", "w") or die("Unable to write to appsettings file");
+            fwrite($setting_file, "pleroma,emoji,".$shortcode.' '.$url);
+            fclose($setting_file);
+        }
+    }
+}
+
+$htmlfile = fopen("$output_filename", "r") or die("Unable to open $output_filename");
+echo fread($htmlfile,filesize("$output_filename"));
+fclose($htmlfile);
+
+?>
